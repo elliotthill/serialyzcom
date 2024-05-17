fce70af52e146473ca486b5542c3decf1874bac8
Elliott Hill <elliott.hill@gmail.com>
2024-05-17 18:54:03 +0100

    Bug: Fix cache clearing cron

--- server/cron/cache.ts
+++ server/cron/cache.ts
@@ -1,16 +1,16 @@
-'use strict'
-import config from "../../config.json" assert {type:"json"};
+import config from "../../config.json" assert {type: "json"}
 
-import {models, sequelize} from '../models/index.js';
-import {QueryTypes} from 'sequelize';
+import { models, sequelize } from "../models/index.js"
+import { QueryTypes } from "sequelize"
 
-const deleteAllOldCaches =  `
+const deleteAllOldCaches = `
                 DELETE
                 FROM render_cache
-                WHERE timestamp < NOW() - INTERVAL :expirationDays DAY`;
+                WHERE timestamp < NOW() - INTERVAL :expirationDays`
 
-sequelize.query(deleteAllOldCaches,
-                {
-                    replacements: {expirationDays:config.CACHE_EXPIRATION_DAYS},
-                    type: QueryTypes.SELECT
-                });
+sequelize
+    .query(deleteAllOldCaches, {
+        replacements: { expirationDays: config.CACHE_EXPIRATION_DAYS + " day" },
+        type: QueryTypes.DELETE
+    })
+    .then(result => process.exit())
